201be93116c956428d5f533b4c4d18881014c76e
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-12-07 16:32:36 +0600

    Implement loading screen component and update Home page to display it during navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
 /** @format */
 "use client";
+import LoadingScreen from "@/components/CommonComponents/LoadingScreen";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const router = useRouter();
-  return router.push("/overview");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+      router.push("/overview");
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
+  return null;
 }
